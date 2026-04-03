c9694947c8e687a9cabf32f674eb3aff6ae3e34a
ShreyaSalve21 <shreyasalve849@gmail.com>
2026-04-03 18:22:21 +0530

    Added Day 3 Linux basics

--- devops/week1/day03_linux_basics/README.md
+++ devops/week1/day03_linux_basics/README.md
@@ -1,1 +1,14 @@
+<<<<<<< HEAD
 
+=======
+# Day 3 - Linux Basics
+ 
+Learned basic Lnux commands and created a shell script.
+
+Commands practiced :
+ - ls, cd, mkdir, touch, cat
+ - grep, find
+ - chmod, ps, top
+
+Created a script to automate file creation and search.
+>>>>>>> ba23826 (Added Day 3 Linux basics)
--- devops/week1/day03_linux_basics/script.sh
+++ devops/week1/day03_linux_basics/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 echo "Creating folder..."
 mkdir myfolder
 
@@ -17,3 +18,22 @@ cat file1.txt
 
 echo "Searching word..."
 grep "World" file1.txt
+=======
+echo "Creating Folder...."
+mkdir myfolder
+
+echo "Going Inside folder.."
+cd myfolder
+
+echo "Creating File..."
+touch file1.txt
+
+echo "Writing content.."
+echo "Hello World" > file1.txt
+
+echo "Displaying content"
+cat file1.txt
+
+echo "Searching word..."
+grep "World" file.txt
+>>>>>>> ba23826 (Added Day 3 Linux basics)
